e58eadc16637befcf59d3a6b5ebfe94246626739
rightbil <rightbil@gmail.com>
2020-02-04 21:25:10 -0500

    final code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,7 @@
 package com.company;
-
-import javax.print.MultiDocPrintService;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -15,25 +14,52 @@ public class Main {
         mapEliza.put("am", "are");
         String[] str; // will hold user input from keyboard
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your sentence");
-        String useInput = sc.nextLine();
-        // will store the input by breaking the empty space
-        str = useInput.split(" ");
-        // will contain every word attaching to the previous value
-        StringBuilder result = new StringBuilder();
-        //go through every word
-        for (int i = 0; i < str.length; i++) {
-            // search the word in the mapEliza hashmap
-            for (String key : mapEliza.keySet()) { // for every key value eg. i , me or my
-                if (str[i].equalsIgnoreCase(key)) { // check if the key value is same as str[i] first word in the input
-                    //for testing purpose only
-                   //System.out.println("@ i=:" + i + " value " + str[i] + " is replaced by " + mapEliza.get(key));
-                    str[i] = mapEliza.get(key);
-                    break;
-                }
+        // control the user if he wants to exit
+        String quit="C";
+        while (!quit.equalsIgnoreCase("Q")) {
+            System.out.println("Enter your sentence");
+            String useInput = sc.nextLine();
+            if(!useInput.equalsIgnoreCase("Q")) {
+                // will store the input by breaking the empty space
+                str = useInput.split(" ");
+                // will contain every word attaching to the previous value
+                StringBuilder result = new StringBuilder();
+                //go through every word
+                for (int i = 0; i < str.length; i++) {
+                    // search the word in the mapEliza hashmap
+                    for (String key : mapEliza.keySet()) { // for every key value eg. i , me or my
+                        if (str[i].equalsIgnoreCase(key)) { // check if the key value is same as str[i] first word in the input
+                            //for testing purpose only
+                            //System.out.println("@ i=:" + i + " value " + str[i] + " is replaced by " + mapEliza.get(key));
+                            str[i] = mapEliza.get(key);
+                            break;
+                        }
+                    }
+                    result.append(str[i] + " ");
+                }// replacement of pronouns is finished till here
+
+                String[] hedges = new String[]{"Please tell me more", "Many of my patients tell me the same thing", "It is getting late, maybe we had better quit"};
+                String[] qualifiers = new String[]{"Why do you say that", "You seem to think that", "So, you are concerned that"};
+                // random hedges and qualifier generator
+                Random randHedges = new Random();
+                int rndHedge = randHedges.nextInt(hedges.length);
+                // need to create a separate Random b/c hedges and qualifiers have different lengths
+                Random randQualifier = new Random();
+                int rndQualifier = randHedges.nextInt(qualifiers.length);
+
+                // let's capture the random values using the random values as index
+                String qualifier = qualifiers[rndQualifier];
+                String hedge = hedges[rndHedge];
+
+                System.out.println(qualifier + " " + result);
+                System.out.println(hedge);
             }
-            result.append(str[i] + " ");
-        }
-        System.out.println(result);
+            else { // this time the user input Q
+                System.out.println("sorry to see you go, have a nice day");
+                quit=useInput;// this means Q so that the outer while loop will not excute again.
+                break;
+            }
+        } // end of while loop
+
     }
 }
